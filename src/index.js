50ca9b2ea9f22afdfc703995813ad441d519d47f
verasunyy <verasun@live.com>
2019-06-07 00:12:28 -0700

    delet unused comment and whitespace

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import './styles/game.css';
-import { SVG_NS, KEYS } from './settings';
 import Game from './partials/Game';
 import StartGame from './partials/StartGame';
-import Board from './partials/Board';
 
+const startButton = document.getElementById("start");
 const startGame = new StartGame('game', 650, 400);
-
-// startGame.initialStart();       
-
-
-
-//start the game
-// function gameStart(){
-  // console.log("gameStar() runs");
-  startGame.start();
-//   console.log("gameStart()"+game.position);
-// console.log("gameStart()"+game.difficalty);
-// console.log("gameStart()"+game.numberOfPlayer);
-  
-// }
-document.getElementById("start").addEventListener("click", function(){
-  document.getElementById("start").classList.remove("show-button");
+startGame.start();
+startButton.addEventListener("click", function(){
+  startButton.classList.remove("show-button");
   const game = new Game('game', startGame);
   ruleLoop();
   function ruleLoop() {
-    // console.log("ruleloop()"+game.position);
-    // console.log("ruleloop()"+game.difficalty);
-    // console.log("ruleloop()"+game.numberOfPlayer);
     if (startGame.restart) {
       console.log("rule loop runs");
       startGame.start();
@@ -35,7 +18,6 @@ document.getElementById("start").addEventListener("click", function(){
         game.render();
         requestAnimationFrame(ruleLoop);
     }
-
   }
 });
 
